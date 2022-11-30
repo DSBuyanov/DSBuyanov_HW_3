e7245416d7b20234535bcbf33f3cc0a9a3560eea
Denis <inquis87@gmail.com>
2022-11-30 22:13:39 +0300

    3 ok

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает
+// число и выдает, является ли число четным (делится ли оно на
+// два без остатка).
+
+Console.Write("Введите число N = ");
+int N = int.Parse(Console.ReadLine());
+
+
+if ((N%2) == 0)
+ {
+   Console.WriteLine("да");
+   }
+else
+    {
+       Console.WriteLine("нет");
+    }
